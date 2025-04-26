f1e8e913f3fe45a317c73a40edd5b91bdcef634b
kirti20081998 <kirtibiswas04@gmail.com>
2025-04-26 09:38:51 +0530

    Add files via upload

---
+++ src/main/java/com/example/myApp/dev.java
@@ -0,0 +1,12 @@
+package com.example.myApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class dev {
+
+	public void build() {
+		System.out.println("Hello Boss");
+	}
+
+}
